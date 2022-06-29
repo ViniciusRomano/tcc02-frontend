b1278c747ecad3f51187b49813032c114dcb8278
ViniciusRomano <romano@alunos.utfpr.edu.br>
2022-06-28 23:20:23 -0300

    first commit

---
+++ src/components/sidebar/NavigationRoutes.js
@@ -0,0 +1,33 @@
+export default {
+  root: {
+    name: '/',
+    displayName: 'navigationRoutes.home',
+  },
+  routes: [
+    {
+      name: 'dashboard',
+      displayName: 'menu.dashboard',
+      meta: {
+        icon: 'vuestic-iconset-dashboard',
+      },
+    },
+    {
+      name: 'statistics',
+      displayName: 'menu.statistics',
+      meta: {
+        icon: 'vuestic-iconset-statistics',
+      },
+      disabled: true,
+      children: [
+        {
+          name: 'charts',
+          displayName: 'menu.charts',
+        },
+        {
+          name: 'progress-bars',
+          displayName: 'menu.progressBars',
+        },
+      ],
+    }
+  ],
+}
---
+++ src/pages/admin/dashboard/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard">
+    <dashboard-charts />
+    <dashboard-info-block />
+
+  </div>
+</template>
+
+<script>
+import DashboardCharts from './DashboardCharts'
+import DashboardInfoBlock from './DashboardInfoBlock'
+import DashboardTabs from './DashboardTabs'
+import DashboardMap from './DashboardMap'
+
+export default {
+  name: 'dashboard',
+  components: {
+    DashboardCharts,
+    DashboardInfoBlock,
+    DashboardTabs,
+    DashboardMap,
+  },
+  methods: {
+    addAddressToMap ({ city, country }) {
+      this.$refs.dashboardMap.addAddress({ city: city.text, country })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+  .row-equal .flex {
+    .va-card {
+      height: 100%;
+    }
+  }
+
+  .dashboard {
+    .va-card {
+      margin-bottom: 0 !important;
+      &__title {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+</style>
